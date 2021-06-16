6b34059defca0a44f80bcf1dcdfe33df5f3f2c4e
galuh.intern <galuh.intern@mail.sirclo.com>
2021-06-17 02:59:13 +0700

    onboard page

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Router, Scene, Stack } from 'react-native-router-flux';
 import HomePage from './pages/home/HomePage';
 import MitraOtwPage from './pages/mitraOtw/MitraOtwPage';
+import LoginPage from './pages/onboard/LoginPage';
+import OnboardPage from './pages/onboard/OnboardPage';
+import RegisterPage from './pages/onboard/RegisterPage';
 import OrderPage from './pages/order/OrderPage';
 import PaymentPage from './pages/payment/PaymentPage';
 
@@ -9,9 +12,12 @@ export default function App() {
   return (
     <Router>
       <Stack key="root">
-        <Scene key="payment" component={PaymentPage}  />
-        <Scene key="mitraOtw" component={MitraOtwPage} hideNavBar />
+        <Scene key="onboard" component={OnboardPage} hideNavBar />
+        <Scene key="register" component={RegisterPage} title="Daftar" back />
+        <Scene key="login" component={LoginPage} title="Login" back />
         <Scene key="home" component={HomePage} hideNavBar />
+        <Scene key="payment" component={PaymentPage} />
+        <Scene key="mitraOtw" component={MitraOtwPage} hideNavBar />
         <Scene key="order" component={OrderPage} title="Service Description" back />
       </Stack>
     </Router>
--- pages/home/HomePage.js
+++ pages/home/HomePage.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
-import { Actions } from 'react-native-router-flux';
 import Banner from './banner';
 import BottomNavigation from './bottomNavigation';
 import Category from './category';
